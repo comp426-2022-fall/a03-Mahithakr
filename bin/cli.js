a5f89ece789e465035dda4db46e9b5bb4346ff33
Mahitha Reddy <mahithareddy@Mahithas-MBP.lan>
2022-11-27 18:12:07 -0500

    initial commit

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+import {roll} from "../lib/roll.js";
+
+const args = minimist(process.argv.slice(2));
+
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const roll = args.rolls || 1;
+
+console.log(roll(sides,dice,rolls));
+process.exit(0);
